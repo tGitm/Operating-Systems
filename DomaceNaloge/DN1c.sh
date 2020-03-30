f38b7b10fcb13410f2281d2b64e44e31a9b3cb17
tGitm <timrus99@gmail.com>
2020-03-30 23:59:43 +0200

    05

--- DomaceNaloge/DN1c.sh
+++ DomaceNaloge/DN1c.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+imenik1="$1"
+imenik2="$2"
+
+if [[ "$imenik2" != '' ]]; then
+    cd "$imenik1" && find . -type d -exec mkdir -p -- "$imenik2"{} \;
+    echo find . -type d
+    if [ $? -eq 0 ];then
+        ln -s "$imenik1" "škorenj"
+    else
+        exit 21
+    fi
+
+    exit 0
+else
+    exit 21
+fi
