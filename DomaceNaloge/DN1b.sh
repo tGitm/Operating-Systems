ca32812762f505c1ad427c5c93b9ffee84f48627
tGitm <timrus99@gmail.com>
2020-04-02 08:13:23 +0200

    comm

--- DomaceNaloge/DN1b.sh
+++ DomaceNaloge/DN1b.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-username="$2"
-
-while getopts 'lha:' OPT; do
-
-    echo "Vnesite geslo za uporabnika $username"
-    read -s pass
+while getopts 'u:-superuser:' OPT; do
+    case $OPT in
+        u) ap=$OPTARG;;
+        -superuser) bp=$OPTARG;;
+    esac
 done
 
+echo "Vnesite geslo za uporabnika $username"
+read -s pass
+#echo $pass
+#spodaj ustvarim uporabnika, z domačim imenikom /home/uporabniško_ime
+uporabnik=$(useradd -m -d /home/$ap $ap)
 
+if [[ "$bp" != '' ]]; then
+    #echo "skripta naredi uporabnika superuserja"
+    usermod -a -G sudo "$uporabnik"
+fi
+if [[ $? -eq 0 ]]; then
+    echo "Uporabnik $ap ustvarjen."
+fi
+#echo $ap
+#echo "$bp"
